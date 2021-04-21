07c114ee2ae03a4cc14786c0b376f825b1b7e5c9
Antônio Carlos <jhunny.tec@gmaill.com>
2021-04-21 16:41:45 -0300

    Adicionando imagens e o exemplo image-deconvolution.py.

---
+++ img/mydata.py
@@ -0,0 +1,28 @@
+from skimage import io
+
+root = "C:/Users/Antonio Carlos/PycharmProjects/scikit-image-examples/img/models/"
+
+
+def beatles():
+    img = io.imread(root + 'beatles.jpg')
+    return img
+
+
+def fire():
+    img = io.imread(root + 'fire.jpg')
+    return img
+
+
+def bird():
+    img = io.imread(root + 'bird.jpg')
+    return img
+
+
+def neymar():
+    img = io.imread(root + 'neymar.jpeg')
+    return img
+
+
+def hollywood():
+    img = io.imread(root + 'hollywood.jpg')
+    return img
